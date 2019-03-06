f8a1300afe0845ad5469f9340cdd45b5cce1d111
phajib <phajib@gmail.com>
2019-03-06 01:04:03 -0800

    Rainbow colors displayed

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
-# Write your #display_rainbow method here
+require 'pry'
+
+def display_rainbow(color)
+    puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}"
+#     rainbow_colors = []
+#     color.each do |rainbow|
+#         rainbow_colors << rainbow[0].upcase + ": " + rainbow
+#      end
+#     binding.pry
+#     p rainbow_colors.join(", ")
+end
